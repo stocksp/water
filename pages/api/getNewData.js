cb6af8985f0711dc3a88596475ecf557032abf36
Paul Stocks <paul.stocks@gmail.com>
2021-07-03 10:35:14 -0700

    getNewData query params for both water and power

--- pages/api/getNewData.js
+++ pages/api/getNewData.js
@@ -3,30 +3,30 @@ import { withMongo } from "libs/mongo";
 
 const handler = async (req, res) => {
   try {
-    console.log("starting getData! since: ", req.query.since);
+    console.log("starting getData! since: ", req.query.water), req.query.power;
     //const dateTest = new Date(req.query.since);
     //console.log('the date:', dateTest.toLocaleTimeString());
     // look back 5 days = 5 * 24 * 60 * 60 * 1000
     const distDocs = await req.db
       .collection("waterDistance")
-      .find({ when: { $gt: new Date(req.query.since) }})
+      .find({ when: { $gt: new Date(req.query.water) }})
       .project({ _id: 0 })
       .sort({ _id: -1 })
       .toArray();
     const powerDocs = await req.db
       .collection("power")
-      .find({ when: { $gt: new Date(req.query.since) }})
+      .find({ when: { $gt: new Date(req.query.power) }})
       .project({ _id: 0 })
       .sort({ _id: -1 })
       .toArray();
-    const voltageDocs = await req.db
+    /* const voltageDocs = await req.db
       .collection("voltage")
-      .find({ when: { $gt: new Date(req.query.since) }})
+      .find({ when: { $gt: new Date(req.query.water) }})
       .project({ _id: 0 })
       .sort({ _id: -1 })
-      .toArray();
-    console.log("found", distDocs.length, powerDocs.length, voltageDocs.length);
-    res.json({ message: "ok", distDocs, powerDocs, voltageDocs });
+      .toArray(); */
+    console.log("found", distDocs.length, powerDocs.length);
+    res.json({ message: "ok", distDocs, powerDocs});
   } catch (error) {
     res.json("Error: " + error.toString());
   }
