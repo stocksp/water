fd165c2e3661d98f8cdb53cb203164c6f2faa811
Paul Stocks <paul.stocks@gmail.com>
2022-09-18 16:43:56 -0700

    query from when to date

--- pages/api/getNewClimate.js
+++ pages/api/getNewClimate.js
@@ -2,19 +2,19 @@ import { withMongo } from "libs/mongo";
 
 const handler = async (req, res) => {
   try {
-    console.log("starting getClimate!");
+    console.log("starting getNewClimate!", req.query.date);
     // look back 5 days = 5 * 24 * 60 * 60 * 1000
     const climateDocs = await req.db
       .collection("climate")
       .find({
-        when: { $gt: new Date(req.query.when) },
+        when: { $gt: new Date(req.query.date) },
         $or: [{ name: "Crawl Space" }, { name: "home" }, { name: "outside" }, { name: "Well Climate inside" }],
       })
       .project({ _id: 0 })
       .sort({ _id: -1 })
       .toArray();
 
-    console.log("found", climateDocs.length, climateDocs[0].when);
+    console.log("found", climateDocs.length, climateDocs[0]);
     res.json({ message: "ok", climateDocs });
   } catch (error) {
     res.json("Error: " + error.toString());
